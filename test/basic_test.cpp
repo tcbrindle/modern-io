ccd48973ccaa69ffd0830fa244adc75d1ed9fccb
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-09 03:14:19 +1200

    Add the most basic of "tests"
    Hello, world!

---
+++ test/basic_test.cpp
@@ -0,0 +1,16 @@
+
+#include "catch.hpp"
+
+#include <io/debug/debug_stream.hpp>
+#include <io/stream_reader.hpp>
+
+#include <iostream>
+
+TEST_CASE("Debug streams can be printed", "[debug]")
+{
+    auto stream = io::debug_stream("Hello, world!\n");
+    auto reader = io::read(std::move(stream));
+
+    io::rng::copy(reader,
+                  io::rng::ostream_iterator<char>(std::cout));
+}
