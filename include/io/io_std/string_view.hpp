03fbc7d7140223dccc8f2a23ed09844aaf1a6ade
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-08 21:30:25 +1200

    Handle __has_include potentially being absent
    I *think* any compiler which can handle Range-V3 is new enough
    that it also has __has_include, but better safe than sorry.

--- include/io/io_std/string_view.hpp
+++ include/io/io_std/string_view.hpp
@@ -2,27 +2,28 @@
 #ifndef IO_STD_STRING_VIEW_HPP
 #define IO_STD_STRING_VIEW_HPP
 
-#if __has_include(<string_view>)
-#include <string_view>
-#define IO_HAVE_STRING_VIEW 1
-namespace io_std {
-    using std::basic_string_view;
-    using string_view = std::string_view;
-    using u16string_view = std::u16string_view;
-    using u32string_view = std::u32string_view;
-    using wstring_view = std::wstring_view;
-}
-#elif __has_include(<experimental/string_view>)
-#include <experimental/string_view>
-#define IO_HAVE_STRING_VIEW 1
-namespace io_std {
-    using std::experimental::basic_string_view;
-    using string_view = std::experimental::string_view;
-    using u16string_view = std::experimental::u16string_view;
-    using u32string_view = std::experimental::u32string_view;
-    using wstring_view = std::experimental::wstring_view;
-}
-#endif // (__has_include(<string_view>) || __has_include(<experimental/string_view>))
-
+#ifdef __has_include
+    #if __has_include(<string_view>)
+        #include <string_view>
+        #define IO_HAVE_STRING_VIEW 1
+        namespace io_std {
+            using std::basic_string_view;
+            using string_view = std::string_view;
+            using u16string_view = std::u16string_view;
+            using u32string_view = std::u32string_view;
+            using wstring_view = std::wstring_view;
+        }
+    #elif __has_include(<experimental/string_view>)
+        #include <experimental/string_view>
+        #define IO_HAVE_STRING_VIEW 1
+        namespace io_std {
+            using std::experimental::basic_string_view;
+            using string_view = std::experimental::string_view;
+            using u16string_view = std::experimental::u16string_view;
+            using u32string_view = std::experimental::u32string_view;
+            using wstring_view = std::experimental::wstring_view;
+        }
+    #endif // (__has_include(<string_view>) || __has_include(<experimental/string_view>))
+#endif // __has_include
 
 #endif // IO_STD_STRING_VIEW_HPP
