f47d3e68c3811689736995b8fbead09a9f653bcc
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-09 14:58:13 +1200

    test: Test return value of read_some
    Also, use a std::string rather than a NTBS

--- test/debug_stream_test.cpp
+++ test/debug_stream_test.cpp
@@ -7,12 +7,12 @@
 #include <range/v3/algorithm.hpp>
 
 #include <array>
-#include <cstring>
+#include <string>
 #include <vector>
 
 namespace rng = ranges::v3;
 
-constexpr auto test_string = "The quick brown fox jumped over the lazy dog";
+const std::string test_string = "The quick brown fox jumped over the lazy dog";
 
 static_assert(io::SyncReadStream<io::debug_stream>(),
               "debug_stream does not meet the SyncReadStream requirements");
@@ -46,7 +46,7 @@ TEST_CASE("Debug streams support seeking", "[debug_stream]")
 
     SECTION("...from end") {
         REQUIRE_NOTHROW(d.seek(-10, io::seek_mode::end));
-        REQUIRE(d.get_position() == std::strlen(test_string) - 10);
+        REQUIRE(d.get_position() == test_string.size() - 10);
     }
 
     SECTION("...from current") {
@@ -65,52 +65,72 @@ TEST_CASE("Debug streams can be read from", "[debug_stream]")
     io::debug_stream d{test_string};
 
     SECTION("...using a short static buffer") {
-        std::array<char, 10> buf;
-        std::error_code ec;
-        REQUIRE_NOTHROW(d.read_some(io::buffer(buf), ec));
+        constexpr int buf_size = 10;
+        std::array<char, buf_size> buf;
+        std::error_code ec{};
+        std::size_t bytes_read = 0;
+        REQUIRE_NOTHROW(bytes_read = d.read_some(io::buffer(buf), ec));
         REQUIRE_FALSE(ec);
-        REQUIRE(rng::equal(buf, test_string));
+        REQUIRE(bytes_read == buf_size);
+        REQUIRE(rng::equal(std::cbegin(buf),
+                           std::cend(buf),
+                           std::cbegin(test_string),
+                           std::cbegin(test_string) + buf_size));
     }
 
     SECTION("...using a short static buffer (throwing)") {
-        std::array<char, 10> buf;
-        REQUIRE_NOTHROW(d.read_some(io::buffer(buf)));
-        REQUIRE(rng::equal(buf, test_string));
+        constexpr int buf_size = 10;
+        std::array<char, buf_size> buf;
+        std::size_t bytes_read = 0;
+        REQUIRE_NOTHROW(bytes_read = d.read_some(io::buffer(buf)));
+        REQUIRE(bytes_read == buf_size);
+        REQUIRE(rng::equal(std::cbegin(buf),
+                           std::cend(buf),
+                           std::cbegin(test_string),
+                           std::cbegin(test_string) + buf_size));
     }
 
     SECTION("...using a long static buffer") {
         std::array<char, 100> buf;
-        std::error_code ec;
-        REQUIRE_NOTHROW(d.read_some(io::buffer(buf), ec));
+        std::error_code ec{};
+        std::size_t bytes_read = 0;
+        REQUIRE_NOTHROW(bytes_read = d.read_some(io::buffer(buf), ec));
         REQUIRE(ec.value() == static_cast<int>(io::stream_errc::eof));
         REQUIRE(ec.category() == io::stream_category());
+        REQUIRE(bytes_read == test_string.size());
         REQUIRE(rng::equal(std::begin(buf),
-                           std::begin(buf) + std::strlen(test_string),
-                           test_string));
+                           std::begin(buf) + test_string.size(),
+                           std::cbegin(test_string),
+                           std::cend(test_string)));
     }
 
     SECTION("...using a long static buffer (throwing)") {
         std::array<char, 100> buf;
-        REQUIRE_THROWS_AS(d.read_some(io::buffer(buf)), std::system_error);
+        std::size_t bytes_read = 0;
+        REQUIRE_THROWS_AS(bytes_read = d.read_some(io::buffer(buf)), std::system_error);
+        REQUIRE(bytes_read == 0); // Because above call doesn't return
         REQUIRE(rng::equal(std::begin(buf),
-                           std::begin(buf) + std::strlen(test_string),
-                           test_string));
+                           std::begin(buf) + test_string.size(),
+                           std::cbegin(test_string),
+                           std::cend(test_string)));
     }
 
     SECTION("...using a dynamic buffer") {
         std::vector<char> buf;
         std::error_code ec;
-        REQUIRE_NOTHROW(io::read(d, io::dynamic_buffer(buf),
-                                 io::transfer_exactly{std::strlen(test_string)},
+        std::size_t bytes_read = 0;
+        REQUIRE_NOTHROW(bytes_read = io::read(d, io::dynamic_buffer(buf),
+                                 io::transfer_exactly{test_string.size()},
                                  ec));
-        REQUIRE(buf.size() == std::strlen(test_string));
+        REQUIRE(bytes_read == test_string.size());
         REQUIRE(rng::equal(buf, test_string));
     }
 
     SECTION("...using a dynamic buffer (throwing)") {
         std::vector<char> buf;
-        REQUIRE_THROWS_AS(io::read(d, io::dynamic_buffer(buf),
-                                   io::transfer_exactly{std::strlen(test_string)}),
+        std::size_t bytes_read = 0;
+        REQUIRE_THROWS_AS(bytes_read = io::read(d, io::dynamic_buffer(buf),
+                                   io::transfer_exactly{test_string.size()}),
                           std::system_error);
         REQUIRE(rng::equal(buf, test_string));
     }
