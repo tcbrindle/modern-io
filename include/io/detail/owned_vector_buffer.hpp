35bc6989adb56eb50a25c979484ed7fbe5ff88d6
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-15 17:54:14 +1200

    buffered_read_stream: Never resize the buffer
    Instead, we maintain offsets to the “start” and “end” of the buffer,
    which we update manually. In effect, we’re using a vector as just a
    stack-allocated array with an allocator, but that’s okay

--- include/io/detail/owned_vector_buffer.hpp
+++ include/io/detail/owned_vector_buffer.hpp
@@ -25,55 +25,52 @@ public:
     using mutable_buffers_type = mutable_buffer;
 
     explicit owned_vector_buffer(std::size_t maximum_size) noexcept
-            : max_size_{maximum_size}
+            : vec_{maximum_size}
     {}
 
     owned_vector_buffer(const Allocator& allocator,
                         size_t maximum_size) noexcept
-            : vec_(allocator), max_size_{maximum_size}
+            : vec_(maximum_size, allocator)
     {}
 
     owned_vector_buffer(owned_vector_buffer&&) = default;
 
     Allocator get_allocator() const { return vec_.get_allocator(); }
 
-    std::size_t size() const noexcept { return size_ - start_; }
+    std::size_t size() const noexcept { return last_ - first_; }
 
-    std::size_t max_size() const noexcept { return max_size_ - start_; }
+    std::size_t max_size() const noexcept { return vec_.size() - first_; }
 
-    std::size_t capacity() const noexcept { return vec_.capacity(); }
+    std::size_t capacity() const noexcept { return vec_.capacity() - first_; }
 
-    const_buffers_type data() const noexcept { return buffer(vec_, size_) + start_; }
+    const_buffers_type data() const noexcept { return buffer(buffer(vec_) + first_, size()); }
 
-    void clear() { start_ = 0; size_ = 0; }
+    void clear() { first_ = 0; last_ = 0; }
 
     mutable_buffers_type prepare(std::size_t n)
     {
-        if (size_ + n > max_size_) {
+        if (last_ + n > max_size()) {
             throw std::length_error{"dynamic_vector_buffer::prepare(): size() + n > max_size()"};
         }
 
-        vec_.resize(size_ + n);
-        return buffer(buffer(vec_) + size_, n);
+        return buffer(buffer(vec_) + last_, n);
     }
 
     void commit(std::size_t n)
     {
-        size_ += std::min(n, vec_.size() - size_);
-        vec_.resize(size_);
+        last_ += std::min(n, vec_.size() - last_);
     }
 
     void consume(std::size_t n)
     {
-        std::size_t m = std::min(n, size_);
-        start_ += m;
+        std::size_t m = std::min(n, last_);
+        first_ += m;
     }
 
 private:
     std::vector<T, Allocator> vec_;
-    std::size_t start_ = 0;
-    std::size_t size_ = 0;
-    const std::size_t max_size_;
+    std::size_t first_ = 0;
+    std::size_t last_ = 0;
 };
 
 
