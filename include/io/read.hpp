8dc580b11fd9bc824a2ca61f6ffc44f3208b0bdd
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-15 22:12:53 +1200

    Fix function declaration order in read.hpp
    Declare (and define) the error code version of read_all() before the
    throwing version. This worked before, but only because everything was
    in a flat io:: namespace so found the second version by ADL.

--- include/io/read.hpp
+++ include/io/read.hpp
@@ -11,24 +11,23 @@ using net::read_until;
 
 template <class SyncReadStream, class DynamicBuffer,
         class = std::enable_if_t<is_dynamic_buffer<std::decay_t<DynamicBuffer>>::value>>
-std::size_t read_all(SyncReadStream& stream, DynamicBuffer&& b)
-{
-    std::error_code ec;
-    std::size_t bytes_read = read_all(stream, std::forward<DynamicBuffer>(b), ec);
-    if (ec) {
-        throw std::system_error{ec};
-    }
-    return bytes_read;
-}
-
-
-template <class SyncReadStream, class DynamicBuffer,
-        class = std::enable_if_t<is_dynamic_buffer<std::decay_t<DynamicBuffer>>::value>>
 std::size_t read_all(SyncReadStream& stream, DynamicBuffer&& b, std::error_code& ec)
 {
     std::size_t bytes_read = io::read(stream, std::forward<DynamicBuffer>(b), ec);
     if (ec == io::make_error_code(stream_errc::eof)) {
         ec.clear();
+    }
+    return bytes_read;
+}
+
+template <class SyncReadStream, class DynamicBuffer,
+        class = std::enable_if_t<is_dynamic_buffer<std::decay_t<DynamicBuffer>>::value>>
+std::size_t read_all(SyncReadStream& stream, DynamicBuffer&& b)
+{
+    std::error_code ec;
+    std::size_t bytes_read = io::read_all(stream, std::forward<DynamicBuffer>(b), ec);
+    if (ec) {
+        throw std::system_error{ec};
     }
     return bytes_read;
 }
