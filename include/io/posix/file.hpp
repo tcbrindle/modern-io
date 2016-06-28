a4c5516db59748758cdc02c0c27d25f512aa5c05
Tristan Brindle <t.c.brindle@gmail.com>
2016-06-29 05:01:53 +1200

    posix_file: Don't inherit from posix::descriptor_stream
    Temporarily, at least, pending a re-think on exactly how to design a
    good hierarchy.

--- include/io/posix/file.hpp
+++ include/io/posix/file.hpp
@@ -3,9 +3,12 @@
 #define IO_POSIX_FILE_HPP
 
 #include <io/io_std/filesystem.hpp>
-#include <io/posix/descriptor_stream.hpp>
+#include <io/posix/file_descriptor_handle.hpp>
 
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 namespace io {
 namespace posix {
@@ -28,8 +31,10 @@ constexpr int seek_mode_to_whence_arg(seek_mode m)
 } // end namespace detail
 
 
-struct file : descriptor_stream {
+class file {
+public:
     using offset_type = ::off_t;
+    using native_handle_type = int;
 
     static file open(const io_std::filesystem::path& path, std::error_code& ec) noexcept
     {
@@ -48,7 +53,145 @@ struct file : descriptor_stream {
     file() = default;
 
     explicit file(posix::file_descriptor_handle fd) noexcept
-            : descriptor_stream{std::move(fd)} {}
+    native_handle_type native_handle() const noexcept { return fd_.get(); }
+
+    // SyncReadStream implementation
+
+    template <typename MutBufSeq,
+            CONCEPT_REQUIRES_(MutableBufferSequence<MutBufSeq>())>
+    std::size_t read_some(const MutBufSeq& mb)
+    {
+        std::error_code ec;
+        auto sz = this->read_some(mb, ec);
+        if (ec) {
+            throw std::system_error{ec};
+        }
+        return sz;
+    }
+
+    template <typename MutBufSeq,
+            CONCEPT_REQUIRES_(MutableBufferSequence<MutBufSeq>())>
+    std::size_t read_some(const MutBufSeq& mb, std::error_code& ec) noexcept
+    {
+        ec.clear();
+
+        if (buffer_size(mb) == 0) {
+            return 0;
+        }
+
+        // Prepare buffers
+        constexpr int max_iovec = 16; //?
+        std::array<::iovec, max_iovec> io_vecs{};
+
+        int i = 0;
+        for (auto it = buffer_sequence_begin(mb);
+             it != buffer_sequence_end(mb); ++it) {
+            io_vecs[i].iov_base = it->data();
+            io_vecs[i].iov_len = it->size();
+            if (++i == max_iovec) {
+                break;
+            }
+        }
+
+        auto bytes = ::readv(native_handle(), io_vecs.data(), i);
+
+        if (bytes == 0) {
+            ec = stream_errc::eof;
+        }
+        else if (bytes < 0) {
+            ec.assign(errno, std::system_category());
+        }
+        else {
+            ec.clear();
+        }
+
+        return bytes;
+    }
+
+    std::size_t read_some(const io::mutable_buffer& mb, std::error_code& ec)
+    {
+        ec.clear();
+
+        if (mb.size() == 0) {
+            return 0;
+        }
+
+        auto bytes_read = ::read(native_handle(), mb.data(), mb.size());
+
+        if (bytes_read == 0) {
+            ec = stream_errc::eof;
+        }
+        else if (bytes_read < 0) {
+            ec.assign(errno, std::system_category());
+        }
+
+        return bytes_read;
+    }
+
+    // SyncWriteStream implementation
+
+    template <typename ConstBufSeq,
+            CONCEPT_REQUIRES_(ConstBufferSequence<ConstBufSeq>())>
+    std::size_t write_some(ConstBufSeq& cb)
+    {
+        std::error_code ec;
+        auto sz = write_some(cb, ec);
+
+        if (ec) {
+            throw std::system_error{ec};
+        }
+
+        return sz;
+    }
+
+    template <typename ConstBufSeq,
+            CONCEPT_REQUIRES_(ConstBufferSequence<ConstBufSeq>())>
+    std::size_t write_some(ConstBufSeq& cb, std::error_code& ec) noexcept
+    {
+        if (io::buffer_size(cb) == 0) {
+            ec.clear();
+            return 0;
+        }
+
+        // Prepare buffers
+        constexpr int max_iovec = 16; //?
+        std::array<::iovec, max_iovec> io_vecs{};
+
+        int i = 0;
+        for (auto it = buffer_sequence_begin(cb);
+             it != buffer_sequence_end(cb); ++it) {
+            io_vecs[i].iov_base = it->data();
+            io_vecs[i].iov_len = it->size();
+            if (++i == max_iovec) {
+                break;
+            }
+        }
+
+        auto bytes_written = ::writev(native_handle(), io_vecs.data(), i);
+
+        if (bytes_written < 0) {
+            ec.assign(errno, std::system_category());
+        }
+
+        return bytes_written;
+    }
+
+    std::size_t write_some(const io::mutable_buffer& cb, std::error_code& ec)
+    {
+        ec.clear();
+
+        if (cb.size() == 0) {
+            return 0;
+        }
+
+        auto bytes_written = ::write(native_handle(), cb.data(), cb.size());
+
+        if (bytes_written < 0) {
+            ec.assign(errno, std::system_category());
+        }
+
+        return bytes_written;
+    }
 
     offset_type seek(offset_type offset, seek_mode from)
     {
@@ -92,6 +235,9 @@ struct file : descriptor_stream {
             throw std::system_error{ec};
         }
     }
+
+private:
+    file_descriptor_handle fd_{};
 };
 
 static_assert(SeekableStream<posix::file>(),
