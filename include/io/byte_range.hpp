8db20c1b719dd2c6ae1527c30a3083cacaa0e4d8
Tristan Brindle <t.c.brindle@gmail.com>
2017-06-11 20:31:24 +0100

    Add byte_range.hpp
    A byte_range is a wrapper which turns a SyncReadStream into an
    InputRange.
    
    For non-buffered ranges, this will be quite hilariously inefficient.

---
+++ include/io/byte_range.hpp
@@ -0,0 +1,113 @@
+
+// Copyright (c) 2017 Tristan Brindle (tcbrindle at gmail dot com)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MODERN_IO_BYTE_RANGE_HPP_INCLUDED
+#define MODERN_IO_BYTE_RANGE_HPP_INCLUDED
+
+#include <io/buffer.hpp>
+#include <io/read.hpp>
+
+namespace io {
+
+template <typename Stream>
+struct byte_range {
+    using stream_type = Stream;
+    using byte_type = unsigned char;
+    struct iterator;
+    using const_iterator = iterator;
+
+    struct iterator {
+        using value_type = byte_type;
+        using difference_type = std::ptrdiff_t;
+        using reference = value_type&;
+        using pointer = value_type*;
+        using iterator_category = std::input_iterator_tag;
+
+        iterator() = default;
+
+        explicit iterator(byte_range* ptr)
+            : range_(ptr)
+        {}
+
+        value_type operator*() const
+        {
+            return range_->val_;
+        }
+
+        iterator& operator++()
+        {
+            range_->next();
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            iterator temp = *this;
+            this->operator++();
+            return temp;
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return done() == other.done() ||
+                    range_ == other.range_;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return !(*this == other);
+        }
+
+    private:
+        bool done() const
+        {
+            return range_ == nullptr || range_->done_;
+        }
+
+        byte_range* range_ = nullptr;
+    };
+
+    byte_range() = default;
+
+    explicit byte_range(Stream& stream)
+        : stream_(std::addressof(stream))
+    {
+        next();
+    }
+
+    iterator begin() { return iterator{this}; }
+    const_iterator begin() const { return const_iterator{this};}
+    const_iterator cbegin() const { return const_iterator{this}; }
+
+    iterator end() { return iterator{}; }
+    const_iterator end() const { return const_iterator{}; }
+    const_iterator cend() const { return const_iterator{}; }
+
+private:
+    void next()
+    {
+        io::mutable_buffer buf(&val_, 1);
+        io::read(*stream_, buf, ec);
+        if (ec) {
+            if (ec == io::stream_errc::eof) {
+                done_ = true;
+            }
+            else  {
+                throw std::system_error(ec);
+            }
+        }
+    }
+
+    stream_type* stream_ = nullptr;
+    std::error_code ec;
+    byte_type val_{};
+    bool done_ = false;
+};
+
+
+
+}
+
+#endif
