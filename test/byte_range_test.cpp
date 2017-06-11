2e431c479d86d6ef720e63f6c8e2c0ec87bcce02
Tristan Brindle <t.c.brindle@gmail.com>
2017-06-11 20:33:04 +0100

    Add skeleton byte_range test

---
+++ test/byte_range_test.cpp
@@ -0,0 +1,17 @@
+
+#include "catch.hpp"
+
+#include <io/string_stream.hpp>
+#include <io/byte_range.hpp>
+
+TEST_CASE("Basic byte_range test")
+{
+    const std::string string = "The quick brown fox jumps over the lazy dog";
+
+    io::string_stream stream(string);
+
+    io::byte_range<io::string_stream> range(stream);
+
+    REQUIRE(std::equal(std::begin(range), std::end(range),
+                       std::begin(string), std::end(string)));
+}
